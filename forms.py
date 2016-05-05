a088dcec830fb62a211a286b768239d6b4293073
Mariah Senaa <senaam@uni.coventry.ac.uk>
2016-05-05 12:03:01 +0000

    updates

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from models import user
+
+ 
+class ContactForm(Form):
+  name = TextField("Name")
+  email = TextField("Email")
+  subject = TextField("Subject")
+  message = TextAreaField("Message")
+  submit = SubmitField("Send")
