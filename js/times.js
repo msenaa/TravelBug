eca90f008c1cc935f0bb9ac00aa782aea97e6eb7
Mariah Senaa <senaam@uni.coventry.ac.uk>
2016-05-06 16:54:58 +0000

    updated website

--- js/times.js
+++ js/times.js
@@ -1,13 +1,7 @@
 // JavaScript File
 
-
-
-    $('.carousel').carousel({
-        interval: 5000 //changes the speed
-    })
-    
 // timezones 
-
+<p id="demo"></p>;
 document.getElementById("demo").innerHTML = 
 new Date("Fri Mar 25 2015 09:56:24 GMT+0100 (Tokyo Time)");
 
